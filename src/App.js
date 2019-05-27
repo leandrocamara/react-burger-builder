a04845e6cb5f831bedaade2023b70658d17a3635
Leandro Câmara <leandrocamara_eng@outlook.com>
2019-05-27 13:32:50 -0300

    ADD - Creating a Layout Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+
 function App() {
   return (
-    <div></div>
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
   );
 }
 
